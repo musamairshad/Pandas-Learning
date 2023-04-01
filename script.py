b0aed364e096dbf3db96cfad672f178181f07c65
Muhammad Usama <imusamairshad@gmail.com>
2023-04-01 15:14:36 +0500

    modified script.py

--- script.py
+++ script.py
@@ -10,13 +10,13 @@ DataFrame from dictionary:
 import pandas as pd
 
 world = {
-    "country" : ["Brazil", "Russia", "India", "China", "South Afria"],
-    "capital" : ["Brasilia", "Moscow", "New Delhi", "Beijing", "Pretoria"],
-    "area" : [8.516, 17.10, 3.286, 9.597, 1.221],
-    "population" : [200.4, 143.5, 1252, 1357, 52.98]
+    "country": ["Brazil", "Russia", "India", "China", "South Afria"],
+    "capital": ["Brasilia", "Moscow", "New Delhi", "Beijing", "Pretoria"],
+    "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+    "population": [200.4, 143.5, 1252, 1357, 52.98]
 }
 
-brics = pd.read_csv("brics.csv", index_col = 0)
+brics = pd.read_csv("brics.csv", index_col=0)
 # read_csv contains many argument's to customize your data import.
 # index_col = 0 means first column contains the row indexes.
 print(brics)
